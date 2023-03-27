62cc7ce8e94507280b6b67a1a9aa5b03146e64e6
leejinuk <lee991227@gmail.com>
2023-03-28 02:28:33 +0900

    Jdbc 리포지토리 구현

--- src/main/java/spring/springintroduction/SpringConfig.java
+++ src/main/java/spring/springintroduction/SpringConfig.java
@@ -1,13 +1,24 @@
 package spring.springintroduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.springintroduction.repository.JdbcMemberRepository;
 import spring.springintroduction.repository.MemberRepository;
 import spring.springintroduction.repository.MemoryMemberRepository;
 import spring.springintroduction.service.MemberService;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
 
     }
 

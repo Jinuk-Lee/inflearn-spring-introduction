0abd67b23e1afc5648b52c809a189399f1c9c840
leejinuk <lee991227@gmail.com>
2023-03-29 18:42:18 +0900

    JPA를 사용하도록 스프링 설정 변경

--- src/main/java/spring/springintroduction/SpringConfig.java
+++ src/main/java/spring/springintroduction/SpringConfig.java
@@ -1,34 +1,31 @@
 package spring.springintroduction;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.springintroduction.repository.JdbcMemberRepository;
-import spring.springintroduction.repository.JdbcTemplateMemberRepository;
-import spring.springintroduction.repository.MemberRepository;
-import spring.springintroduction.repository.MemoryMemberRepository;
+import spring.springintroduction.repository.*;
 import spring.springintroduction.service.MemberService;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+
     }
 
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+//return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
 
     }
 

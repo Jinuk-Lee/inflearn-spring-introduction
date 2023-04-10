00e333b88aa1613854786d11f7f382c702a03f34
leejinuk <lee991227@gmail.com>
2023-04-10 23:55:04 +0900

    실행 파일

--- src/main/java/spring/springintroduction/SpringIntroductionApplication.java
+++ src/main/java/spring/springintroduction/SpringIntroductionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringIntroductionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(SpringIntroductionApplication.class, args);
 	}
 
